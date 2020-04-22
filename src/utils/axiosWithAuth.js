1874b9496129428b51a651b065ccc079e1ac1ba0
Steven <38477361+Steven-matos@users.noreply.github.com>
2020-04-22 17:59:57 -0400

    provided correct HTTPS link in axiosWithAuth

--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -4,10 +4,10 @@ const axiosWithAuth = () => {
   const token = localStorage.getItem("token");
 
   return axios.create({
-    baseURL: "http://groabe-env.v3umry9g8h.us-east-1.elasticbeanstalk.com/api/users",
+    baseURL: "https://dev.groa.us/api/users",
     headers: {
-      Authorization: token
-    }
+      Authorization: token,
+    },
   });
 };
 
