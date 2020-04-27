0a4318589ccb919dc9c7dede5ca6b24d79e79115
Steven <38477361+Steven-matos@users.noreply.github.com>
2020-04-27 15:56:30 -0400

    passed down movie id to movie card

--- src/components/dashboard/Explore.js
+++ src/components/dashboard/Explore.js
@@ -7,22 +7,22 @@ import { getMoviesAction, setFilter } from "../../store/actions/index.js";
 import MovieCard from "../movies/MovieCard.js";
 import LoadingScreen from "../layout/LoadingScreen.js";
 
-function Explore({ 
-  isFetching, 
-  movies, 
-  userid, 
-  getMoviesAction, 
-  searchTerm, 
+function Explore({
+  isFetching,
+  movies,
+  userid,
+  getMoviesAction,
+  searchTerm,
   setFilter,
-  ratings 
+  ratings,
 }) {
   useEffect(() => {
-    setFilter("")
+    setFilter("");
     // Returns the movies
     getMoviesAction(userid);
   }, [getMoviesAction, userid, ratings, setFilter]);
   // How many movies render
-  const cardAmount = 25
+  const cardAmount = 25;
 
   if (isFetching) return <LoadingScreen />;
   else
@@ -30,58 +30,59 @@ function Explore({
       <div className="container explore" data-test={ifDev("ratings-component")}>
         <div className="movie-cards">
           {movies
-          .filter( movie =>
-            !ratings.includes(film => 
-              film.primary_title === movie.primary_title && film.start_year === movie.start_year
-            ).length &&
-            searchTerm !== '' ? 
-              movie.primary_title
-              .toString()
-              .toLowerCase()
-              .includes(
-                searchTerm
-                .toLowerCase()
-              ) 
-            : true
-          )
-          .slice(0, cardAmount)
-          .map((movie, index) => {
-            /* Checks if the film is in ratings */
-            const isRated = film => {
-              return film.primary_title === movie.primary_title && film.start_year === movie.start_year
-            }
-            /* Returns the movie object if in ratings */
-            let rated = ratings.find(isRated)
-            let posterURI = movie.poster_url;
-            let unsplashUrl =
-              "https://source.unsplash.com/collection/1736993/500x650";
-            let moviePoster = `https://image.tmdb.org/t/p/w500${posterURI}?api_key=${process.env.REACT_APP_TMDB_API_KEY}`;
+            .filter((movie) =>
+              !ratings.includes(
+                (film) =>
+                  film.primary_title === movie.primary_title &&
+                  film.start_year === movie.start_year
+              ).length && searchTerm !== ""
+                ? movie.primary_title
+                    .toString()
+                    .toLowerCase()
+                    .includes(searchTerm.toLowerCase())
+                : true
+            )
+            .slice(0, cardAmount)
+            .map((movie, index) => {
+              /* Checks if the film is in ratings */
+              const isRated = (film) => {
+                return (
+                  film.primary_title === movie.primary_title &&
+                  film.start_year === movie.start_year
+                );
+              };
+              /* Returns the movie object if in ratings */
+              let rated = ratings.find(isRated);
+              let posterURI = movie.poster_url;
+              let unsplashUrl =
+                "https://source.unsplash.com/collection/1736993/500x650";
+              let moviePoster = `https://image.tmdb.org/t/p/w500${posterURI}?api_key=${process.env.REACT_APP_TMDB_API_KEY}`;
 
-            return (
-              <MovieCard
-                key={index}
-                name={movie.primary_title}
-                year={movie.start_year}
-                rated={rated ? rated.rating : null}
-                image={
-                  !posterURI ||
-                  posterURI === "None" ||
-                  posterURI === "No poster" ||
-                  posterURI === "No Poster" ||
-                  posterURI === "Not in table"
-                    ? unsplashUrl
-                    : moviePoster
-                }
-              />
-            );
-          })
-        }
+              return (
+                <MovieCard
+                  key={index}
+                  name={movie.primary_title}
+                  year={movie.start_year}
+                  movie_id={movie.movie_id}
+                  rated={rated ? rated.rating : null}
+                  image={
+                    !posterURI ||
+                    posterURI === "None" ||
+                    posterURI === "No poster" ||
+                    posterURI === "No Poster" ||
+                    posterURI === "Not in table"
+                      ? unsplashUrl
+                      : moviePoster
+                  }
+                />
+              );
+            })}
+        </div>
       </div>
-    </div>
-  );
+    );
 }
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
     userid: state.login.userid,
     isFetching: state.movie.isFetching,
@@ -89,7 +90,9 @@ const mapStateToProps = state => {
     moviesError: state.movie.error,
     searchTerm: state.filter.searchTerm,
     watchlist: state.watchlist.movies,
-    ratings: state.rating.movies
+    ratings: state.rating.movies,
   };
 };
-export default connect(mapStateToProps, { getMoviesAction, setFilter })(Explore);
+export default connect(mapStateToProps, { getMoviesAction, setFilter })(
+  Explore
+);
