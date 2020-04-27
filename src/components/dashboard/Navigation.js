433ea514525f5edde09273ceb7f6e116358b9b47
Juana Villalpando <juana.v14@gmail.com>
2020-04-27 01:21:23 -0700

    add side navbar and user drop down menu updated

--- src/components/dashboard/Navigation.js
+++ src/components/dashboard/Navigation.js
@@ -20,15 +20,47 @@ import { ifDev } from "../../utils/removeAttribute.js";
 import GroaLogo from "../../img/groa-logo-nav.png";
 
 //for search bar 
-import { makeStyles, fade } from "@material-ui/core/styles";
+import { makeStyles, fade, useTheme } from "@material-ui/core/styles";
 import SearchIcon from "@material-ui/icons/Search";
 import InputBase from "@material-ui/core/InputBase";
+
+//for the navbar 
+import clsx from 'clsx';
+// import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+//for user toggle menu
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import PersonRoundedIcon from '@material-ui/icons/PersonRounded';
+
+const drawerWidth = 240;
 
 
 const useStyles = makeStyles(theme => ({
   searchContainer: {
-    width: "46%",
-    margin: "auto"
+    width: "70%",
+    margin: "0 auto"
+  },
+  [theme.breakpoints.down("xs")]: {
+    searchContainer:{
+      width:"90%",
+    }
   },
   search: {
     position: "relative",
@@ -63,13 +95,126 @@ const useStyles = makeStyles(theme => ({
     [theme.breakpoints.up("md")]: {
       width: 200
     }
+  },
+  //for nav bar
+  root: {
+    display: 'flex',
+    flexDirection:'column'
+  },
+  appBar: {
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  hide: {
+    display: 'none',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: -drawerWidth,
+  },
+  contentShift: {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
+  },
+  toolBar:{
+    backgroundColor:'#505050'
+  },
+  groaLogo: {
+    height:'2rem',
+    width: 'auto',
+  },
+  logoContainer:{
+    display:'flex',
+    flexDirection:'row'
+  },
+  drawerContainer:{
+    display:'flex',
+    flexDirection:'column',
   }
 }));
 
 
 const Navigation = props => {
-  const classes = useStyles();
+  //for nav bar
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+  // for side bar menu
+  const linkAttributes = [{
+    name: "Upload",
+    icon: <InboxIcon /> 
+  },
+  {
+    name: "Explore",
+    icon: <MailIcon /> 
+  },
+  {
+    name: "Recommended",
+    icon: <InboxIcon /> 
+  },
+  {
+    name: "Wishlist",
+    icon: <InboxIcon /> 
+  },
+  {
+    name: "Ratings",
+    icon: <InboxIcon /> 
+  }];
 
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+  //for user icon menu
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const classes = useStyles();
   const [query, setQuery] = useState({
     query: ""
   });
@@ -102,137 +247,50 @@ const Navigation = props => {
     props.recommendationAction(id);
   };
 
+
   return (
-    <div className="mainContainer" data-test={ifDev("navigation")}>
-      <div className="navContainer">
-        <div className="Bars hidden">
-          <FontAwesomeIcon className="bars-icon" icon={faBars} />
-          <i className="far fa-bars"></i>
-        </div>
-
-        <div className="Links">
-          <img src={GroaLogo} alt="Groa Logo" />
-
-          <NavLink
-            className="NavLink recommended"
-            to={`/${props.userid}/recommended`}
+    <div className={classes.root}>
+      <div className={classes.drawerContainer}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: open,
+        })}
+      >
+        <Toolbar className={classes.toolBar}>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            className={clsx(classes.menuButton, open && classes.hide)}
           >
-            Recommended
-          </NavLink>
-
-          <NavLink
-            className="NavLink  watchlist"
-            to={`/${props.userid}/watchlist`}
-          >
-            Watchlist
-          </NavLink>
-
-          <NavLink className="NavLink explore" to={`/${props.userid}/explore`}>
-            Explore
-          </NavLink>
-        </div>
-
-        {/* If the path is recommended show update recommendations button */}
-        <button
-          className={`recommendations-button ${
-            window.location.pathname === `/${props.userid}/recommended`
-              ? null
-              : ` hidden`
-          }`}
-          onClick={() => getNewRecommendations(props.userid)}
-        >
-          <FontAwesomeIcon className="sync-icon fa-fw" icon={faSync} />
-          <i className="fas fa-sync"></i>
-          Update your recs
-        </button>
-
-        <div className="fa-icons">
-          {/* This is the container for the user-icon and the arrow */}
-          <div className="dropdown-hover">
-            <FontAwesomeIcon className="user-circle-icon" icon={faUserCircle} />
-            <i className="far fa-user-circle"></i>
-
-            <FontAwesomeIcon className="angle-down-icon" icon={faAngleDown} />
-            <i className="fas fa-angle-down"></i>
-
-            <div className="dropdown-content">
-              {/* This is the dropdown menu, links display based on media query */}
-              <NavLink
-                className="NavLink recommended-menu"
-                to={`/${props.userid}/recommended`}
-              >
-                Recommended
-              </NavLink>
-
-              <NavLink
-                className="NavLink explore-menu"
-                to={`/${props.userid}/explore`}
-              >
-                Explore
-              </NavLink>
-
-              <NavLink
-                className="NavLink ratings-menu"
-                to={`/${props.userid}/ratings`}
-              >
-                Ratings
-              </NavLink>
-
-              <NavLink
-                className="NavLink upload-menu"
-                to={`/${props.userid}/upload`}
-              >
-                Upload data
-              </NavLink>
-
-              <NavLink
-                className="NavLink watchlist-menu"
-                to={`/${props.userid}/watchlist`}
-              >
-                Watchlist
-              </NavLink>
-
-              <NavLink
-                className="NavLink logout-menu"
-                onClick={logout}
-                data-test={ifDev("logoutBtn")}
-                to="/login"
-              >
-                Log out
-              </NavLink>
-            </div>
-            {/* END dropdown-content */}
-          </div>
-          {/* END dropdown-hover */}
-        </div>
-        {/* END fa-icons */}
-      </div>
-      {/* <div
-          className={`searchContainer ${
-            window.location.pathname === `/${props.userid}/upload`
-              ? `hidden`
-              : null
-          }`}
-        >
-          <div className="search-wrapper">
-            <FontAwesomeIcon className="search-icon" icon={faSearch} />
-            <input
-              className="searchBox"
-              type="text"
-              name="search"
-              value={query.query}
-              onChange={handleChange}
-              placeholder="Search..."
-              onKeyDown={handleSubmit}
-            />
-          </div>
-          <FontAwesomeIcon
-            className="backspace-icon"
-            icon={faBackspace}
-            onClick={clearInput}
-          />
-        </div> */}
-         <div className={classes.searchContainer}>
+            <MenuIcon />
+          </IconButton>
+          <div className={classes.logoContainer}>
+          <Typography variant="h6" noWrap>
+          <img className={classes.groaLogo} src={GroaLogo} alt="Groa Logo" />
+          </Typography>
+          
+      <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+      <PersonRoundedIcon/>
+      </Button>
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleClose} button component="a" href={`/${props.userid}/ratings`} >Ratings</MenuItem>
+        <MenuItem onClick={handleClose} button component="a" href={`/${props.userid}/upload`}>Upload</MenuItem>
+        <MenuItem onClick={logout} button component="a" href={`/login`}>Logout</MenuItem>
+      </Menu>
+      
+    </div>
+    
+    <div className={classes.searchContainer}>
               <div className={classes.search}>
                 <div className={classes.searchIcon}>
                   <SearchIcon />
@@ -250,8 +308,53 @@ const Navigation = props => {
                   onKeyDown={handleSubmit}
                 />
               </div>
-            </div>
-      {/* END navContainer */}
+            
+    </div>
+
+     
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        className={classes.drawer}
+        variant="persistent"
+        anchor="left"
+        open={open}
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <div className={classes.drawerHeader}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+        </div>
+        <Divider />
+        <List>
+          {linkAttributes.map((linkObject, index) => (
+            <ListItem button component="a" href={`/${props.userid}/${linkObject.name.toLowerCase()}`}>
+              <ListItemIcon>{linkObject.icon}</ListItemIcon>
+              <ListItemText primary={linkObject.name} />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        {/* <List>
+          {['All mail', 'Trash', 'Spam'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List> */}
+      </Drawer>
+      <main
+        className={clsx(classes.content, {
+          [classes.contentShift]: open,
+        })}
+      >
+        <div className={classes.drawerHeader} />
+      </main>
+      </div>
     </div>
   );
 };
