3517384ccb28cebae86769feb4957de8245bf7e5
Steven <38477361+Steven-matos@users.noreply.github.com>
2020-04-28 14:16:01 -0400

    removed recommended action that is not used

--- src/components/dashboard/Recommendations.js
+++ src/components/dashboard/Recommendations.js
@@ -3,10 +3,9 @@ import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { ifDev } from "../../utils/removeAttribute.js";
 import {
-  recommendedAction,
   recommendationAction,
   toggleIsUploaded,
-  setFilter
+  setFilter,
 } from "../../store/actions/index.js";
 // children components
 import LoadingScreen from "../layout/LoadingScreen.js";
@@ -16,23 +15,20 @@ function Recommendations({
   isFetching,
   recommendations,
   userid,
-  recommendedAction,
   recommendationAction,
-  searchTerm, 
+  searchTerm,
   isUploaded,
-  setFilter
+  setFilter,
 }) {
   useEffect(() => {
-    setFilter("")
-    // returns initial recommendations after uploading a file
+    setFilter("");
     if (isUploaded === true) {
       recommendationAction(userid);
       toggleIsUploaded();
     }
+    recommendationAction(userid);
     // Returns the most recent recommendations from the database
-    recommendedAction(userid);
-
-  }, [ recommendedAction, userid, isUploaded, recommendationAction, setFilter ]);
+  }, [userid, isUploaded, recommendationAction, setFilter]);
 
   if (isFetching) return <LoadingScreen />;
   else
@@ -43,62 +39,57 @@ function Recommendations({
       >
         <div className="movie-cards">
           {recommendations
-          .filter( movie => 
-            searchTerm !== '' ? 
-              movie.Title
-              .toString()
-              .toLowerCase()
-              .includes(
-                searchTerm
-                .toLowerCase()
-              ) 
-            : true 
-          )
-          .map((movie, index) => {
-            let posterURI = movie["Poster URL"];
-            let unsplashUrl =
-              "https://source.unsplash.com/collection/1736993/500x650";
-            let moviePoster = 
-              `https://image.tmdb.org/t/p/w500${posterURI}?api_key=${process.env.REACT_APP_TMDB_API_KEY}`;
+            .filter((movie) =>
+              searchTerm !== ""
+                ? movie.title
+                    .toString()
+                    .toLowerCase()
+                    .includes(searchTerm.toLowerCase())
+                : true
+            )
+            .map((movie, index) => {
+              let posterURI = movie.poster_url;
+              let unsplashUrl =
+                "https://source.unsplash.com/collection/1736993/500x650";
+              let moviePoster = `https://image.tmdb.org/t/p/w500${posterURI}?api_key=${process.env.REACT_APP_TMDB_API_KEY}`;
 
-            return (
-              <MovieCard
-                key={index}
-                rated={null}
-                name={movie.Title}
-                year={movie.Year}
-                image={
-                  !posterURI ||
-                  posterURI === "None" ||
-                  posterURI === "No poster" ||
-                  posterURI === "No Poster" ||
-                  posterURI === "Not in table"
-                    ? unsplashUrl
-                    : moviePoster
-                }
-              />
-            );
-          })
-        }
+              return (
+                <MovieCard
+                  key={index}
+                  rated={null}
+                  name={movie.title}
+                  year={movie.year}
+                  movie_id={movie.movie_id}
+                  image={
+                    !posterURI ||
+                    posterURI === "None" ||
+                    posterURI === "No poster" ||
+                    posterURI === "No Poster" ||
+                    posterURI === "Not in table"
+                      ? unsplashUrl
+                      : moviePoster
+                  }
+                />
+              );
+            })}
+        </div>
       </div>
-    </div>
-  );
+    );
 }
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
     userid: state.login.userid,
     isFetching: state.recommendations.isFetching,
     recommendations: state.recommendations.movies,
     recommendationsError: state.recommendations.error,
     searchTerm: state.filter.searchTerm,
-    isUploaded: state.upload.isUploaded
+    isUploaded: state.upload.isUploaded,
   };
 };
 
 export default connect(mapStateToProps, {
-  recommendedAction,
   recommendationAction,
   toggleIsUploaded,
-  setFilter
+  setFilter,
 })(Recommendations);
