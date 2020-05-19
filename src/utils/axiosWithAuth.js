37ae1ac4590222c688ac1b65b417c7e71739c98d
Luis PM <luisp237@gmail.com>
2020-05-19 16:26:43 -0400

    axios baseURL updated to env variable instead of localhost

--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -6,7 +6,7 @@ const axiosWithAuth = (accessToken) => {
         headers: {
             Authorization: `Bearer ${accessToken}`
         },
-        baseURL: 'http://localhost:8080/api/users',
+        baseURL: process.env.REACT_APP_BASEURL
     })
 }
 export default axiosWithAuth;
