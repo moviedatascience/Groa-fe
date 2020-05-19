26da1296b4a330393d8bd90cec14541d266b660f
Steven <38477361+Steven-matos@users.noreply.github.com>
2020-05-19 19:02:24 -0400

    removed unused code

--- src/components/auth/Onboarding.js
+++ src/components/auth/Onboarding.js
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
-import { getMoviesAction, getRatingAction, setFilter } from "../../store/actions/index.js";
+import {
+  getMoviesAction,
+  getRatingAction,
+  setFilter,
+} from "../../store/actions/index.js";
 // Screen width util
 import widthFinder from "../../utils/widthFinder.js";
 // children components
@@ -16,8 +20,7 @@ import MuiAlert from "@material-ui/lab/Alert";
 
 //okta
 import { useOktaAuth } from "@okta/okta-react/dist/OktaContext";
-import { Redirect } from 'react-router';
-
+import { Redirect } from "react-router";
 
 const useStyles = makeStyles((theme) => ({
   cardGrid: {
@@ -30,10 +33,10 @@ const useStyles = makeStyles((theme) => ({
     marginBottom: "2%",
   },
   movieCard: {
-    color: 'white',
+    color: "white",
     "&:hover": {
-      boxShadow: '0px 0px 2px 2px black',
-      backgroundColor: 'black',
+      boxShadow: "0px 0px 2px 2px black",
+      backgroundColor: "black",
     },
   },
 }));
@@ -42,18 +45,17 @@ function Alert(props) {
   return <MuiAlert elevation={6} variant="filled" {...props} />;
 }
 
-function Onboarding(
-  {
-    isFetching,
-    movies,
-    userid,
-    getMoviesAction,
-    searchTerm,
-    setFilter,
-    ratings,
-  },
-  props
-) {
+function Onboarding({
+  isFetching,
+  movies,
+  userid,
+  getMoviesAction,
+  searchTerm,
+  setFilter,
+  ratings,
+  queries,
+  isSearching,
+}) {
   // console.log("movies", movies);
   const classes = useStyles();
   //for okta
@@ -63,18 +65,9 @@ function Onboarding(
   const [openAlert, setOpenAlert] = useState(false);
   let [numRatings, setNumRatings] = useState({ num: 0 });
   const screenWidth = widthFinder(window.innerWidth);
-  //for search bar
-  const [query, setQuery] = useState({
-    query: "",
-  });
-
-  const sendChange = (query) => {
-    props.setFilter(query);
-  };
 
   const handleClickStar = () => {
     setOpenAlert(true);
-
   };
   const handleCloseStar = (event, reason) => {
     if (reason === "clickaway") {
@@ -87,83 +80,84 @@ function Onboarding(
     setFilter("");
     // Returns the movies
     getMoviesAction(userid, accessToken);
-  }, [getMoviesAction, userid, ratings, setFilter,]);
+  }, [getMoviesAction, userid, ratings, setFilter, accessToken]);
   // How many movies render
   const cardAmount = 25;
 
   if (isFetching) return <LoadingScreen />;
-  else if (numRatings.num >= 13) return <Redirect to='postonboarding' />
-  else return (
-    <div>
-      <GridList
-        className={classes.cardGrid}
-        cols={screenWidth ? 2 : 5}
-        cellHeight="auto"
-      >
-        {movies
-          .filter((movie) =>
-            !ratings.includes(
-              (film) =>
-                film.primary_title === movie.primary_title &&
-                film.start_year === movie.start_year
-            ).length && searchTerm !== ""
-              ? movie.primary_title
-                .toString()
-                .toLowerCase()
-                .includes(searchTerm.toLowerCase())
-              : true
-          )
-          .slice(0, cardAmount)
-          .map((movie, index) => {
-            /* Checks if the film is in ratings */
-            const isRated = (film) => {
+  else if (numRatings.num >= 13) return <Redirect to="postonboarding" />;
+  else
+    return (
+      <div>
+        <GridList
+          className={classes.cardGrid}
+          cols={screenWidth ? 2 : 5}
+          cellHeight="auto"
+        >
+          {movies
+            .filter((movie) =>
+              !ratings.includes(
+                (film) =>
+                  film.primary_title === movie.primary_title &&
+                  film.start_year === movie.start_year
+              ).length && searchTerm !== ""
+                ? movie.primary_title
+                    .toString()
+                    .toLowerCase()
+                    .includes(searchTerm.toLowerCase())
+                : true
+            )
+            .slice(0, cardAmount)
+            .map((movie, index) => {
+              /* Checks if the film is in ratings */
+              const isRated = (film) => {
+                return (
+                  film.primary_title === movie.primary_title &&
+                  film.start_year === movie.start_year
+                );
+              };
+              /* Returns the movie object if in ratings */
+              let rated = ratings.find(isRated);
+              let posterURI = movie.poster_url;
+              let unsplashUrl =
+                "https://source.unsplash.com/collection/1736993/500x650";
+              let moviePoster = `https://image.tmdb.org/t/p/w500${posterURI}?api_key=${process.env.REACT_APP_TMDB_API_KEY}`;
               return (
-                film.primary_title === movie.primary_title &&
-                film.start_year === movie.start_year
-              );
-            };
-            /* Returns the movie object if in ratings */
-            let rated = ratings.find(isRated);
-            let posterURI = movie.poster_url;
-            let unsplashUrl =
-              "https://source.unsplash.com/collection/1736993/500x650";
-            let moviePoster = `https://image.tmdb.org/t/p/w500${posterURI}?api_key=${process.env.REACT_APP_TMDB_API_KEY}`;
-            return (
-              <div className={classes.movieCard}>
-                <MovieCard
-                  key={index}
-                  name={movie.primary_title}
-                  page={"Onboarding"}
-                  movie_id={movie.movie_id}
-                  rated={rated ? rated.rating : null}
-                  image={
-                    !posterURI ||
+                <div className={classes.movieCard}>
+                  <MovieCard
+                    key={index}
+                    name={movie.primary_title}
+                    page={"Onboarding"}
+                    movie_id={movie.movie_id}
+                    rated={rated ? rated.rating : null}
+                    image={
+                      !posterURI ||
                       posterURI === "None" ||
                       posterURI === "No poster" ||
                       posterURI === "No Poster" ||
                       posterURI === "Not in table"
-                      ? unsplashUrl
-                      : moviePoster
-                  }
-                  handleClickStar={handleClickStar}
-                  numRatings={numRatings}
-                  setNumRatings={setNumRatings}
-                />
-              </div>
-            );
-          })}
-      </GridList>
-      <Snackbar
-        open={openAlert}
-        autoHideDuration={4000}
-        onClose={handleCloseStar}
-      >
-        <Alert onClose={handleCloseStar} variant="filled" severity="success">
-          You have successfully rated this movie!
-        </Alert>
-      </Snackbar>
-    </div>
-  );
+                        ? unsplashUrl
+                        : moviePoster
+                    }
+                    handleClickStar={handleClickStar}
+                    numRatings={numRatings}
+                    setNumRatings={setNumRatings}
+                  />
+                </div>
+              );
+            })}
+        </GridList>
+        <Snackbar
+          open={openAlert}
+          autoHideDuration={4000}
+          onClose={handleCloseStar}
+        >
+          <Alert onClose={handleCloseStar} variant="filled" severity="success">
+            You have successfully rated this movie!
+          </Alert>
+        </Snackbar>
+      </div>
+    );
 }
 
 const mapStateToProps = (state) => {
@@ -176,11 +170,12 @@ const mapStateToProps = (state) => {
     watchlist: state.watchlist.movies,
     ratings: state.rating.movies,
     ratingsError: state.rating.error,
+    queries: state.search.queries,
+    isSearching: state.search.isSearching,
   };
 };
 export default connect(mapStateToProps, {
   getMoviesAction,
   setFilter,
-  getRatingAction
+  getRatingAction,
 })(Onboarding);
-
