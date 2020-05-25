bf888f92b445c129d2a36945ba4b0090c3493ea8
Luis PM <luisp237@gmail.com>
2020-05-25 18:13:14 -0400

    axios base url changed to variable

--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -1,12 +1,14 @@
 import axios from 'axios';
 
+const baseURL = process.env.REACT_APP_BASEURL || 'http://localhost:8080/api/users'
+
 const axiosWithAuth = (accessToken) => {
 
     return axios.create({
         headers: {
             Authorization: `Bearer ${accessToken}`
         },
-        baseURL: 'http://localhost:8080/api/users'
+        baseURL: baseURL
     })
 }
 export default axiosWithAuth;
