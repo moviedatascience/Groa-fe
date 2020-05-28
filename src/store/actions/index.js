8d6e6121676d9db4b7907df2d2e459fcaf3bea15
Steven <38477361+Steven-matos@users.noreply.github.com>
2020-05-28 15:35:26 -0400

    added remove ratings action

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,5 +1,9 @@
 import { loginAction } from "./loginAction.js";
-import { ratingAction, getRatingAction } from "./ratingAction.js";
+import {
+  ratingAction,
+  getRatingAction,
+  removeRatingAction,
+} from "./ratingAction.js";
 import { registerAction } from "./registerAction.js";
 import { recommendationAction } from "./recommendationActions.js";
 import { uploadAction, toggleIsUploaded } from "./uploadAction.js";
@@ -8,18 +12,18 @@ import {
   removeFromWatchlistAction,
   getWatchlistAction,
 } from "./watchlistActions.js";
-import { getMoviesAction } from "./movieAction"
+import { getMoviesAction } from "./movieAction";
 import { setFilter } from "./filterActions.js";
-import { notWatchListAction } from './notWatchListAction.js';
-import { serviceProviderAction } from './serviceProviderAction.js';
+import { notWatchListAction } from "./notWatchListAction.js";
+import { serviceProviderAction } from "./serviceProviderAction.js";
 import { searchAction } from "./searchAction.js";
-import { 
+import {
   horrorLandingAction,
   comedyLandingAction,
   dramaLandingAction,
   romanceLandingAction,
-  staffLandingAction 
- } from "./landingPageAction";
+  staffLandingAction,
+} from "./landingPageAction";
 
 export {
   loginAction,
@@ -41,5 +45,6 @@ export {
   comedyLandingAction,
   dramaLandingAction,
   romanceLandingAction,
-  staffLandingAction 
+  staffLandingAction,
+  removeRatingAction,
 };
